30e9c37e9bb834ca878c939765b3b22d9db3f6ce
Mert-Akar <123328879+Mert-Akar@users.noreply.github.com>
2023-12-30 15:43:15 +0300

    Update main.py
    $random now quotes message author along with message content

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import discord
-import os
-import re
 from discord.ext import commands
-from random import randint
+from random import choice
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,9 +11,8 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def random(ctx):
     with open("out_of_context_messages.txt", 'r', encoding = "utf-8") as message_file:
          messages = list(message_file.read().split("\n\n"))
-         message_count = len(messages)
-         random_index = randint(0,message_count - 1)
-         await ctx.send(messages[random_index])
+         random_message = choice(messages)
+         await ctx.send(random_message)
 
 
 @bot.command()
@@ -23,7 +20,7 @@ async def add(ctx):
     if ctx.message.reference is not None:
         message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
         with open("out_of_context_messages.txt", 'a', encoding = "utf-8") as message_file:
-                message_file.write(f'\n\n{message.content}')
+                message_file.write(f'\n\n{message.content} - {message.author}') ## quotes message along with message author
                 await ctx.send("Message has been added to database.")
     else:
         await ctx.send("You must reply to the message you wish to add.")
